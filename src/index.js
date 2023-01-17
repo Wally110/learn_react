fec7c0be9481a7b85ba09e4512c55629b4308f7b
shiguang <shigzhang@tesla.com>
2023-01-17 14:16:37 +0800

    deploying to Github Pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from './Api' // './App'
+import App from './App' // './Api'
 import './index.css'
 
 
+// In development environment, we've been compiling, hot-reloading and updating on the fly.
+// For production, we're going to have `static files` loading in - none of the source code.
+// We can do this by making a build and deploying it.
 
+// run `npm run build`
+// This will create a `build` folder which will contain the app. Put the contents of that folder anywhere.
 ReactDOM.render(<App />, document.getElementById('root'))
